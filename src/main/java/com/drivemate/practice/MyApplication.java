faf88ceb620b39249cee43a87b1777e2d7a5b107
Sreedevi <sreedevig95@gmail.com>
2023-01-18 13:04:53 +0530

    search feature

--- src/main/java/com/drivemate/practice/MyApplication.java
+++ src/main/java/com/drivemate/practice/MyApplication.java
@@ -8,11 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class MyApplication {
-//    @RequestMapping("/")
-//    String home() {
-//        return "Hello Rimmy!";
-//    }
+    @RequestMapping("/register")
+    String register(String studentName, Integer studentAge) {
 
+        return "Student of name " +studentName+ " and age " +studentAge+ "has been registered";
+    }
+
+    @RequestMapping("/List")
+    String List(){
+        return "hello";
+    }
+
+
+    @RequestMapping("/search")
+        String search(String studentSearch) {
+        return studentSearch+" is present";
+    }
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
